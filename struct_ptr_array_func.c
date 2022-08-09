7ee6123e9687d072e4dbd074ca4c7f161e6f2231
Sayantan Barai <99691210+barai96@users.noreply.github.com>
2022-08-09 21:10:22 +0530

    Update struct_ptr_array_func.c

--- struct_ptr_array_func.c
+++ struct_ptr_array_func.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+
+//creating a structure for student list consisting of a string of Name, integer of Roll number and  float of marks
 struct student
 {
 char name[30];
@@ -13,34 +15,34 @@ void display(struct student *);
 int main()
 {
     int n,i; 
-    printf("Enter strength \n");
+    printf("Enter strength \n");        //entering the length of the array
     scanf("%d",&n);
-    struct student s[n];
-    for (i=0;i<n;i++)
+    struct student s[n];                //defining a array of structure of the given length
+    for (i=0;i<n;i++)                   //loop untill the end of the structure to enter the details of student, call by referance, passes as the adress of the elements
       {
         enter(&s[i]);
       }
       
-    for (i=0;i<n;i++)
+    for (i=0;i<n;i++)                    //loop untill the end of the structure to show the details of student, call by referance, passes as the adress of the elements
       {
           display(&s[i]);
       }
     return 0;
 }
 
-void enter(struct student *ptr)
+void enter(struct student *ptr)         //taking the adress of the array of structure to the pointer *ptr to access the structure objects
 {
   printf("Name\n");
-  scanf("%s",ptr->name);
+  scanf("%s",ptr->name);                //dereferancing to thr members of the array of structure using pointer
   printf("Roll\n");
   scanf("%d",&ptr->roll);
   printf("Marks\n");
   scanf("%f",&ptr->marks);
 }
 
-void display(struct student *ptr)
+void display(struct student *ptr)       //taking the adress of the array of structure to the pointer *ptr to access the structure objects
 {
-  printf("Name %s\n",ptr->name);
+  printf("Name %s\n",ptr->name);        //dereferancing to thr members of the array of structure using pointer
   printf("Roll %d\n",ptr->roll);
   printf("Marks %f\n",ptr->marks);
 }
